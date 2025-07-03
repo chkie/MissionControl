e73ef7e95d6ab0d7a3703e2baad65985d423cdf7
Kevin Kienle <kevinkienle@web.de>
2025-07-03 16:01:01 +0200

    feat: add an object in the Program.cs file  with the values Name, Status, Startzeitpunkt and output it on the console with Console.WriteLine. import namespace MissionControl to Program.cs file

--- Mission.cs
+++ Mission.cs
@@ -7,6 +7,14 @@ namespace MissionControl
 {
     public class Mission
     {
+        public Mission() { }
+
+        public Mission(string missionName, string missionStatus)
+        {
+            Name = missionName;
+            Status = missionStatus;
+        }
+
         public string? Name { get; set; }
         public DateTime Startzeitpunkt { get; set; }
         public string? Status { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Mission Control Online!");
+﻿
+using MissionControl;
+
+
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Mission Control Online!");
+        Console.WriteLine("=======================\n");
+
+        var mission = new Mission
+        {
+            Name = "Test Start",
+            Startzeitpunkt = new DateTime(2025, 07, 05, 12, 00, 00),
+            Status = "Bereit",
+        };
+
+        Console.WriteLine("Mission:");
+        Console.WriteLine($"Name: \t\t{mission.Name}");
+        Console.WriteLine($"Startzeitpunkt: {mission.Startzeitpunkt}");
+        Console.WriteLine($"Status: \t{mission.Status}");
+    }
+}
